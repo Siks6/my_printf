5caaec6a7c348dc67fd2d49a7ec91aaac313cabe
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-04-19 15:20:37 -0700

    c string

--- printf.c
+++ printf.c
@@ -11,11 +11,22 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0;
+
 	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
+		if (format[i] != '%')
 		_putchar(format[i]);
+
+		else
+		{
+			/* i.e. when -> format[i] == % */
+			if (format[i+1] == 'c')
+				_putchar(va_arg(args, int));
+		
+		}
+
 		i++;
 	}
 	va_end(args);
--- test/main.c
+++ test/main.c
@@ -1,4 +1,5 @@
 #include "../main.h"
+#include <stdio.h>
 /** 
  * main - funct to test code 
  *
@@ -6,8 +7,8 @@
  */
 int main(void)
 {
-	_printf("Hello world\n");
-	printf("Hello world\n");
+	_printf("Hello %c world\n", 'B');
+	printf("Hello %c world\n", 'B');
 
 	return (0);
 }
